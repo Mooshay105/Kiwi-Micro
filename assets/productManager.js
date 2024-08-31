5498be3437452156fe45ce471fa5319fb91a53e9
Malcolm Hauser <hauser.malcolm@gmail.com>
2024-08-31 12:43:21 +1000

    code cleanup

--- assets/productManager.js
+++ assets/productManager.js
@@ -1,15 +1,11 @@
 const kCameraCameraNoNoIRPrice = "$90";
 const kCameraCameraNoIRPrice = "$100";
-
 const kStoreStorage256gbPrice = "kStore 256GB Coming Soon!";
 const kStoreStorage512gbPrice = "kStore 512GB Coming Soon!";
-
 const kCameraCameraNoNoIRObject = document.getElementById("kCameraCameraNoNoIR");
 const kCameraCameraNoIRObject = document.getElementById("kCameraCameraNoIR");
-
 const kStoreStorage256gbObject = document.getElementById("kStoreStorage256gb");
 const kStoreStorage512gbObject = document.getElementById("kStoreStorage512gb");
-
 const configPrice = document.getElementById("configPrice");
 const kCameraProductFlag = document.getElementById("kCamera");
 const kStoreProductFlag = document.getElementById("kStore");
@@ -24,46 +20,34 @@ if (pageTitle == "Kiwi Micro | kStore") {
 	configPrice.innerHTML = kStoreStorage256gbPrice;
 }
 
-function selectProduct(productID) {
-	if (productID == "kCameraCameraNoNoIR") {
-		kCameraCameraX("NoNoIR");
-	} else if (productID == "kCameraCameraNoIR") {
-		kCameraCameraX("NoIR");
-	} else if (productID == "kStoreStorage256gb") {
-		kStoreStorageX(256);
-	} else if (productID == "kStoreStorage512gb") {
-		kStoreStorageX(512);
-	}
-}
+kCameraCameraNoIRObject.addEventListener("click", function () {
+	configPrice.innerHTML = kCameraCameraNoIRPrice;
+	kCameraCameraNoNoIRObject.classList.remove("configButtonActive");
+	kCameraCameraNoNoIRObject.classList.add("configButton");
+	kCameraCameraNoIRObject.classList.remove("configButton");
+	kCameraCameraNoIRObject.classList.add("configButtonActive");
+});
 
-function kCameraCameraX(storageSize) {
-	if (storageSize == "NoNoIR") {
-		configPrice.innerHTML = kCameraCameraNoNoIRPrice;
-		kCameraCameraNoNoIRObject.classList.add("configButtonActive");
-		kCameraCameraNoIRObject.classList.remove("configButtonActive");
-		kCameraCameraNoIRObject.classList.add("configButton");
-		kCameraCameraNoNoIRObject.classList.remove("configButton");
-	} else if (storageSize == "NoIR") {
-		configPrice.innerHTML = kCameraCameraNoIRPrice;
-		kCameraCameraNoNoIRObject.classList.remove("configButtonActive");
-		kCameraCameraNoNoIRObject.classList.add("configButton");
-		kCameraCameraNoIRObject.classList.remove("configButton");
-		kCameraCameraNoIRObject.classList.add("configButtonActive");
-	}
-}
+kCameraCameraNoNoIRObject.addEventListener("click", function () {
+	configPrice.innerHTML = kCameraCameraNoNoIRPrice;
+	kCameraCameraNoNoIRObject.classList.add("configButtonActive");
+	kCameraCameraNoIRObject.classList.remove("configButtonActive");
+	kCameraCameraNoIRObject.classList.add("configButton");
+	kCameraCameraNoNoIRObject.classList.remove("configButton");
+});
 
-function kStoreStorageX(storageSize) {
-	if (storageSize == 256) {
-		configPrice.innerHTML = kStoreStorage256gbPrice;
-		kStoreStorage256gbObject.classList.add("configButtonActive");
-		kStoreStorage512gbObject.classList.remove("configButtonActive");
-		kStoreStorage512gbObject.classList.add("configButton");
-		kStoreStorage256gbObject.classList.remove("configButton");
-	} else if (storageSize == 512) {
-		configPrice.innerHTML = kStoreStorage512gbPrice;
-		kStoreStorage256gbObject.classList.remove("configButtonActive");
-		kStoreStorage256gbObject.classList.add("configButton");
-		kStoreStorage512gbObject.classList.remove("configButton");
-		kStoreStorage512gbObject.classList.add("configButtonActive");
-	}
-}
+kStoreStorage256gbObject.addEventListener("click", function () {
+	configPrice.innerHTML = kStoreStorage256gbPrice;
+	kStoreStorage256gbObject.classList.add("configButtonActive");
+	kStoreStorage512gbObject.classList.remove("configButtonActive");
+	kStoreStorage512gbObject.classList.add("configButton");
+	kStoreStorage256gbObject.classList.remove("configButton");
+});
+
+kStoreStorage512gbObject.addEventListener("click", function () {
+	configPrice.innerHTML = kStoreStorage512gbPrice;
+	kStoreStorage256gbObject.classList.remove("configButtonActive");
+	kStoreStorage256gbObject.classList.add("configButton");
+	kStoreStorage512gbObject.classList.remove("configButton");
+	kStoreStorage512gbObject.classList.add("configButtonActive");
+});
