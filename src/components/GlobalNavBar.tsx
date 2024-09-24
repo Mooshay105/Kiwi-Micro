cb7978748098d0a57eaa346248cdf2d4ed49e8e3
Malcolm <hauser.malcolm@gmail.com>
2024-09-24 19:33:20 +1000

    Simplify Deployment

--- src/components/GlobalNavBar.tsx
+++ src/components/GlobalNavBar.tsx
@@ -1,16 +1,25 @@
-import logo from "../assets/img/logo.png";
+import logo_kiwi from "../assets/img/logo-kiwi.png";
+import logo_kangaroo from "../assets/img/logo-kangaroo.png";
 
 let pageTitle = document.title;
 let fullPageURL = window.location.href;
 let newPageTitle;
 let pageURL = fullPageURL.split("/").slice(0, 3).join("/");
+let navLink: string;
+let logo: string;
 
-if (pageURL === "https://kiwi-micro.com") {
+if (pageURL === "https://kiwi-micro.com" || pageURL === "http://kiwi-micro.com") {
 	newPageTitle = "Kiwi Micro | " + pageTitle;
-} else if (pageURL === "https://kangaroo-micro.com") {
+	navLink = "https://kiwi-micro.com";
+	logo = logo_kiwi;
+} else if (pageURL === "https://kangaroo-micro.com" || pageURL === "http://kangaroo-micro.com") {
 	newPageTitle = "Kangaroo Micro | " + pageTitle;
+	navLink = "https://kangaroo-micro.com";
+	logo = logo_kangaroo;
 } else {
 	newPageTitle = "Micro Dev of All Micros | " + pageTitle;
+	navLink = "http://localhost:5173/";
+	logo = logo_kiwi;
 }
 
 document.title = newPageTitle;
@@ -22,9 +31,10 @@ interface GlobalNavBarProps {
 function GlobalNavBar({ blur }: GlobalNavBarProps) {
 	return (
 		<nav className={blur ? "navBlur" : ""}>
+			<link rel="icon" href={logo} />
 			<div className="navContent">
 				<ul className="navList">
-					<a href="https://kiwi-micro.com" className="navLink">
+					<a href={navLink} className="navLink">
 						<img src={logo} width="30px" height="30px" alt="Logo" />
 					</a>
 					<a href="rfid.html" className="navLink">
