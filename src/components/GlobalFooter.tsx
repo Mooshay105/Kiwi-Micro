5769db2259fe091e8e59f6bcc158b81400fcfbb3
Malcolm <hauser.malcolm@gmail.com>
2024-09-29 09:34:43 +1000

    depricate `kangaroo-micro.com`

--- src/components/GlobalFooter.tsx
+++ src/components/GlobalFooter.tsx
@@ -1,20 +1,7 @@
 function GlobalFooter() {
-	let text = "Copyright © 2024 Micro Dev of All Micros";
-	let fullURL = window.location.href;
-
-	let URL = fullURL.split("/").slice(0, 3).join("/");
-
-	if (URL === "https://kiwi-micro.com") {
-		text = "Copyright © 2024 Kiwi Micro";
-	}
-
-	if (URL === "https://kangaroo-micro.com") {
-		text = "Copyright © 2024 Kangaroo Micro";
-	}
-
 	return (
 		<footer>
-			<p>{text}</p>
+			<p>Copyright © 2024 Kiwi Micro</p>
 		</footer>
 	);
 }
