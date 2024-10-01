9fd99d02854e2d5253ccf3442377387c0dbbe362
Malcolm <hauser.malcolm@gmail.com>
2024-10-01 13:39:59 +1000

    add an about me

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import GlobalNavBar from "../components/GlobalNavBar";
+import CustomBreak from "../components/CustomBreak";
 import GlobalFooter from "../components/GlobalFooter";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -19,11 +20,20 @@ function Index() {
 							<h1>Kiwi Micro</h1>
 							<p>Top-Tier Web Development Lives Right Here</p>
 							<a href="gallery.html" className="btn">
-								See Our Code
+								See Our Projects
 							</a>
 						</div>
 					</div>
 				</section>
+				<CustomBreak height={1} />
+				<div className="aboutMe">
+					<img src="/assets/img/malcolm.png" width="200px" height="200px" />
+					<div className="column">
+						<h1>Hi, I'm Malcolm</h1>
+						<p>I am a 13 year-old living in Australia! I like to code in HTML, CSS, JavaScript, Java and a little C.</p>
+					</div>
+				</div>
+				<CustomBreak height={1} />
 				<GlobalFooter />
 			</div>
 		</React.StrictMode>
