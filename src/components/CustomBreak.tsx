533fa32c9f2bd55a94c43c6b60f4a7d70e01ccc5
Malcolm <hauser.malcolm@gmail.com>
2024-10-02 17:21:16 +1000

    Update the Custom Break Element

--- src/components/CustomBreak.tsx
+++ src/components/CustomBreak.tsx
@@ -1,15 +1,21 @@
 interface CustomBreakProps {
 	height: number;
 	hasHR?: boolean;
+	paddBothSides?: boolean;
 }
 
-function CustomBreak({ height, hasHR = false }: CustomBreakProps) {
+function CustomBreak({ height, hasHR = false, paddBothSides = false }: CustomBreakProps) {
 	let br = "";
 	for (let i = 0; i < height; i++) {
 		br += "<br />";
 	}
 	if (hasHR) {
 		br += "<hr />";
+	}
+	if (paddBothSides) {
+		for (let i = 0; i < height; i++) {
+			br += "<br />";
+		}
 	}
 	return <span dangerouslySetInnerHTML={{ __html: br }}></span>;
 }
